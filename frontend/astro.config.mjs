f148f86484162b8214f73efd9e8dfe1742417958
Oliver Evans <oliverevans96@gmail.com>
2023-01-25 21:37:53 -0800

    Update site url in astro config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pureintentionsinc.com",
+  site: "https://pi-web-u2wshzqega-uc.a.run.app",
   integrations: [tailwind()],
   output: "server",
   adapter: node({mode: "middleware"})
