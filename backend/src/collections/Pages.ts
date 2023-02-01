047571ea84b4f8575ddcc0a99aa64ef8cdc1282c
Oliver Evans <oliverevans96@gmail.com>
2023-02-01 02:26:59 -0800

    Support generating pages from payload blocks.

---
+++ backend/src/collections/Pages.ts
@@ -0,0 +1,66 @@
+import { CollectionConfig } from 'payload/types';
+import ProductCardBlock from '../blocks/ProductCard';
+import ParagraphBlock from '../blocks/Paragraph';
+import HeaderBlock from '../blocks/Header';
+
+const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    defaultColumns: ['title', 'author', 'category', 'tags', 'status'],
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      // TODO: validate
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'category',
+      type: 'relationship',
+      relationTo: 'categories'
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: [
+        {
+          value: 'draft',
+          label: 'Draft',
+        },
+        {
+          value: 'published',
+          label: 'Published',
+        },
+      ],
+      defaultValue: 'draft',
+      admin: {
+        position: 'sidebar',
+      }
+    },
+    {
+      name: 'blocks', // required
+      label: 'Content Blocks',
+      type: 'blocks', // required
+      minRows: 1,
+      maxRows: 20,
+      blocks: [ // required
+        ProductCardBlock,
+        ParagraphBlock,
+        HeaderBlock,
+      ]
+    }
+  ],
+}
+
+export default Pages;
