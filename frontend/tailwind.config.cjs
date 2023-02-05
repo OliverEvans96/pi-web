58dc1c0e1bc8d27a6c5ef11beba48e169638ef7c
HAILEY STEINEKE <hailey.steineke@gmail.com>
2023-02-04 21:03:17 -0800

    styled EventCards and working header colors--tailwind custom colors NOT working, but standard colors are

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,13 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			'purple': '#6f2dbdff',
-		},
 		fontFamily: {
 			sans: ['Poppins', 'sans-serif']
-		}
-		extend: {},
+		},
+		extend: {
+			
+			colors: {
+				'newpurple': '#FFC0CB',
+			},
+		},
 	},
 	plugins: [],
 }
